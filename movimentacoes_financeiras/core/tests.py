a4b38b62d45d339ce0d04cda853f5b7d8a404b63
Felipe Gomes Duarte <xfelipegd@gmail.com>
2021-09-05 00:13:50 -0300

    View principal criada em /.

--- movimentacoes_financeiras/core/tests.py
+++ movimentacoes_financeiras/core/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.test import Client
+from django.urls import resolve
 
 # Create your tests here.
+
+class TestMainPage(TestCase):
+    def test_get_main_page_has_status_code_200(self):
+        c = Client()
+        response = c.get('/')
+        self.assertEquals(response.status_code, 200)
--- movimentacoes_financeiras/core/views.py
+++ movimentacoes_financeiras/core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
 # Create your views here.
+def read_file(request):
+    return HttpResponse('Hello world!')
